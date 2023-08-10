8d70ed2bffa93523faad731312d92611ff591d5a
Deadwoodxbruh <aakash.gudivada@gmail.com>
2023-08-10 18:36:32 +0530

    k
    k

---
+++ login.js
@@ -0,0 +1,45 @@
+const button = document.getElementById("submit");
+let check = true;
+const namegiven = document.getElementById("username");
+const p = document.getElementById("password");
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+button.addEventListener("click", function () {
+    if (check === true) {
+        if (p.value !== "" && namegiven.value !== "") {
+            check = false;
+            button.textContent = "Logging in..";
+            const urlgiven = "https://meluhamun.aakashgudivada.repl.co/getlogin?username=" + namegiven.value + "&p=" + p.value;
+            console.log(urlgiven);
+            fetch(urlgiven)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success === true && data.password == true) {
+                        window.location.href = "dashboard.html"
+                    }else {
+                        button.textContent = "Invalid username/password";
+                        button.style.backgroundColor = "red";
+                        namegiven.value = "";
+                        p.value = "";
+                    }
+                })
+                .finally(() => {
+                    check = true;
+                    setTimeout(() => {
+                        button.textContent = "Login"
+                        button.style.backgroundColor = "green"
+                    }, 2000);
+                });
+        } else {
+            button.textContent = "Please enter username and password";
+            button.style.backgroundColor = "orange";
+        }
+    }
+});
