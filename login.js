5ddfc8abe35bad002cfa17cb053fafd7ab020998
Deadwoodxbruh <aakash.gudivada@gmail.com>
2023-08-10 18:43:39 +0530

    kkkkkk
    kkk

--- login.js
+++ login.js
@@ -3,14 +3,6 @@ let check = true;
 const namegiven = document.getElementById("username");
 const p = document.getElementById("password");
 
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-}
-
 button.addEventListener("click", function () {
     if (check === true) {
         if (p.value !== "" && namegiven.value !== "") {
@@ -43,3 +35,11 @@ button.addEventListener("click", function () {
         }
     }
 });
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
