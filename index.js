bfb752a20296699fde70afd10145ff4961c2bd82
Deadwoodxbruh <aakash.gudivada@gmail.com>
2023-08-15 21:31:21 +0530

    k

--- index.js
+++ index.js
@@ -1,60 +1,71 @@
 const dgabutton = document.getElementById("dga");
 const alb = document.getElementById("atr");
+const pja = document.getElementById("pja");
 
 const gba = document.getElementById("ipa");
-let status1 = null;
+let statusretireved = null;
+
+async function getdatastatus(id){
+    if (statusretireved === null){
+        try{
+            const response = await fetch("https://meluhamun.aakashgudivada.repl.co/getstatus?type=" + id)
+            if (!response.ok){
+                throw new Error("welp")
+            }
+            const data = await response.json();
+            statusretireved = data;
+            return data;
+        }catch(error){
+            console.log(error);
+            throw error;
+        }
+    }else{
+        return statusretireved
+    }
+}
 
 alb.addEventListener('click',function(){
-    console.log("sd");
     window.location.href = "Allocationhtml.html";
 });
 
-dgabutton.addEventListener("click",function(){
-    if (status1 === null){
-        fetch("https://meluhamun.aakashgudivada.repl.co/registerstatusda")
-    .then(response => {
-        dgabutton.textContent = "Loading...";
-        return response.json();
-    })
-    .then(data => {
-        const statusofapps = data.status;
-        setTimeout(() => {
-            if (statusofapps === true){
-                window.location.href = "https://youtube.com"
-            }else{
-                dgabutton.textContent = "Delegate Applications not open yet"
-                setTimeout(() => {
-                    dgabutton.textContent = "Delegate Applications"
-                }, 3000);
-            }
-        }, 1000);
-    });
-    }else{
-        if (status1 === true){
-            window.href.location = "https:"
+dgabutton.addEventListener("click",async function(){
+    dgabutton.textContent = 'Loading..';
+    try{
+        const apidata = await getdatastatus("1");
+        if (apidata.status === true){
+            dgabutton.textContent = "Loading.."
         }else{
-            dgabutton.textContent = "Delegate Applications not open yet"
-        }
+            dgabutton.textContent = "Delegate Applications are not open yet!";
+        };
+    }catch(error){
+        console.log(error);
     }
 });
 
-gba.addEventListener("click",function(){
-    fetch("https://meluhamun.aakashgudivada.repl.co/registerstatuspa")
-    .then(response => {
-        dgabutton.textContent = "Loading...";
-        return response.json();
-    })
-    .then(data => {
-        const statusofapps = data.status;
-        setTimeout(() => {
-            if (statusofapps === true){
-                window.location.href = "https://youtube.com"
-            }else{
-                gbaabutton.textContent = "Delegate Applications not open yet"
-                setTimeout(() => {
-                    gbabutton.textContent = "Delegate Applications"
-                }, 3000);
-            }
-        }, 1000);
-    });
+pja.addEventListener("click",async function(){
+    pja.textContent = 'Loading..';
+    try{
+        const apidata = await getdatastatus("3");
+        if (apidata.status === true){
+            pja.textContent = "Loading.."
+        }else{
+            pja.textContent = "PhotoJournalism Applications are not open yet!";
+        };
+    }catch(error){
+        console.log(error);
+    }
+});
+
+gba.addEventListener("click",async function(){
+    gba.textContent = 'Loading..';
+    try{
+        const apidata = await getdatastatus("2");
+        if (apidata.status === true){
+            gba.textContent = "Loading.."
+        }else{
+            gba.textContent = "International Press Applications are not open yet!";
+        };
+    }catch(error){
+        console.log(error);
+    }
 });
